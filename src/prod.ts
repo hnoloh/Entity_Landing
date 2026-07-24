12fccb7223bbe038892cbcfce5c8a17849ab055d
hnoloh <hnoloh@example.com>
2026-07-24 10:12:51 +0200

    fix: express v5 catch-all route

--- src/prod.ts
+++ src/prod.ts
@@ -25,7 +25,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // Cualquier otra ruta la redirigimos a index.html (modo SPA)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
